98fdf838e3425eec98fd548844d3f333d5091f3b
thirteenggh <thirteenggh@gmail.com>
2025-01-21 14:59:00 +0800

    Clean up redundant files

--- 0.3.x/0.3.6/resources/jupyter/extensions/tooling-extension/jupyter_tooling/open-tools-widget.js
+++ 0.3.x/0.3.6/resources/jupyter/extensions/tooling-extension/jupyter_tooling/open-tools-widget.js
@@ -129,6 +129,7 @@ define([
         }
       }
 
+      tools_menu_items +='<li> <a id="upload_model" role="menuitem" tabindex="-1" href="https://distri.ai/dashboard" target="_blank" rel="noreferrer">Upload model or file<span style="display: block; color: #gray; padding: initial; font-size: 12px;">Upload your trained model or file to the Distri market</span></a> </li>'
       docs_button = '<form action="https://github.com/distri-group"  target="_blank" style="display: inline-block;margin-left: 2px;"><button class="btn btn-default btn-sm"> <i class="fa-github fa"></i> Docs</button></form>'
 
       tools_dropwdown =
@@ -137,8 +138,6 @@ define([
                 <span>Open Tool</span> <span class="caret"></span> </button> \
                 <ul id="start-tool" class="dropdown-menu" style="right: 0; left: auto;">' +
         tools_menu_items +
-        '<li><div style="display: block;padding: 3px 20px;clear: both;font-weight: 400;line-height: 1.42857143;color: #333333;white-space: nowrap;"><a href="https://github.com/distri-group">Upload model or file</a><span style="display: block; color: #gray; padding: initial; font-size: 12px;"><div>Upload your trained model or file to the Distri market</div></span></div></li>' +
-        " </ul> </div>" +
         '<li role="presentation" class="divider"></li>' +
         '<li><div style="display: block;padding: 3px 20px;clear: both;font-weight: 400;line-height: 1.42857143;color: #333333;white-space: nowrap;"><div>Do you like DistriAI?</div><span style="display: block; color: #gray; padding: initial; font-size: 12px;"><a href="https://github.com/distri-group" style="color: #333333;font-weight: 550;" target="_blank">Give us a star</a> or <a href="https://twitter.com/DistriAI_web3" style="color: #333333;font-weight: 550;" target="_blank">follow us on Twitter!</a></span></div></li>' +
         " </ul> </div>";        
